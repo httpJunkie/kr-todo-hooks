d77980724b41fd258380e049330b9be4b9caf756
Eric Bishard <e.bishard@gmail.com>
2019-08-02 20:09:02 -0700

    Add Sass files for themes directly inside the App.scss file nested under respective app-container.dark vs app-container.light bam...

--- app/App.js
+++ app/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useMediaPredicate } from 'react-media-hook';
+
 
 import './App.scss';
 import '@progress/kendo-theme-material/dist/all.css';
@@ -8,50 +7,13 @@ import '@progress/kendo-react-intl';
 
 import { AppProvider } from "./AppContext";
 
-import Home from './view-components/Home';
-import Todos from './view-components/todos/Todos';
-import Todoz from './view-components/todos/Todoz';
-
-import SideNav from './partial-components/Sidenav';
-import TopNav from './partial-components/Topnav';
-import Footer from './Footer';
-
-import Theme from './Theme';
+import Main from './Main';
 
 const App = () => {
-  let isMediumPlus = useMediaPredicate("(min-width: 600px)") ? false : true;
 
   return (
     <AppProvider>
-      <Theme />
-      <BrowserRouter>
-        <div className={`app-container ${!isMediumPlus ? 'medium' : 'small'} `}>
-          <main>
-
-            <header>
-              <div className="logo">
-                <span className="k-icon k-i-check"></span> the-todo.co
-              </div>
-              <TopNav />
-            </header>
-
-            <section>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/todos" component={Todos} />
-                <Route exact path="/todoz" component={Todoz} />
-                <Route render={() => <h2 className="four-o-four">404 Page Not Found</h2>} />
-              </Switch>
-            </section>
-
-            <footer>
-              <Footer />
-            </footer>
-
-          </main>
-          <SideNav />
-        </div>
-      </BrowserRouter>
+      <Main />
     </AppProvider>
   )
 }
--- app/Main.js
+++ app/Main.js
@@ -1,14 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useMediaPredicate } from 'react-media-hook';
 import { AppContext } from "./AppContext";
 
-const Footer = () => {
+import Home from './view-components/Home';
+import Todos from './view-components/todos/Todos';
+import Todoz from './view-components/todos/Todoz';
+
+import SideNav from './partial-components/Sidenav';
+import TopNav from './partial-components/Topnav';
+import Footer from './Footer';
+
+const Main = () => {
   const context = useContext(AppContext);
+  let isMediumPlus = useMediaPredicate("(min-width: 600px)") ? false : true;
 
   return (
     <div>
+      <BrowserRouter>
+        <div
+          className={`app-container ${!isMediumPlus ? "medium" : "small"} 
+            ${context.themeMode === 'light' ? 'light' : 'dark'}`}>
+          <main>
+            <header>
+              <div className="logo">
+                <span className="k-icon k-i-check" /> the-todo.co
+              </div>
+              <TopNav />
+            </header>
 
+            <section>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/todos" component={Todos} />
+                <Route exact path="/todoz" component={Todoz} />
+                <Route
+                  render={() => (
+                    <h2 className="four-o-four">404 Page Not Found</h2>
+                  )}
+                />
+              </Switch>
+            </section>
+
+            <footer>
+              <Footer />
+            </footer>
+          </main>
+          <SideNav />
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
-export default Footer;
+export default Main;
