ec1b94cff2581068c29b380992091a9290f76150
Eric Bishard <e.bishard@gmail.com>
2019-08-09 07:23:48 -0700

    Add local storage support for theme mode
    Check local storage for theme before setting in AppContext
    
    Manually manage local storage value for theme, afterwards we can try using a `useLocalStorage` hook

--- app/context/AppContext.js
+++ app/context/AppContext.js
@@ -1,20 +1,24 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+const ls_theme_key = 'kr_todo_theme'; // local storage key for theme
 
 const AppContext = createContext();
 const AppProvider = (props) => {
-
   const defaultState = {
     navOpen: false,
-    themeMode: 'light',
+    themeMode: localStorage.getItem(ls_theme_key) || 'light',
     toggleSidenav: (value) => {
       setApp((data) => ({ ...data, navOpen: value }));
     },
     changeTheme: (mode) => {
-      setApp((data) => ({ ...data, themeMode: mode }));
+      setApp((data) => ({...data, themeMode: mode }));
     }
   }
-  const [appData, setApp] = useState(defaultState);
 
+  const [appData, setApp] = useState(defaultState);
+  useEffect(() => {
+    localStorage.setItem(ls_theme_key, appData.themeMode);
+  }, [appData.themeMode]);
+  
   return (
     <AppContext.Provider value={appData}>
       {props.children}
