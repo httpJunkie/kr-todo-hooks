2b488d752703e839f20e6613f94c0cfa42e1c7bf
Eric Bishard <e.bishard@gmail.com>
2019-08-21 07:10:08 -0700

    Move SendSuccessMessage() into function calls Cleanup HTML and format for demo use

--- app/view-components/todos/Todos.js
+++ app/view-components/todos/Todos.js
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useReducer,
-  useRef,
-  useContext,
-  useEffect
-} from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import "@progress/kendo-theme-material/dist/all.css";
 
 import * as constants from "./constants";
@@ -20,13 +14,10 @@ const Todo = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
   const [textInput, setTextInput] = useState("");
   const completedTodos = todos.filter(todo => todo.complete);
-  //message that will be sent to screen reader upon successful completion of an action
   const [successMsg, setSuccessMsg] = useState("");
   document.title = `The To Do List`;
 
-  useEffect(() => {
-    document.title = `${completedTodos.length} completed to do's`;
-  });
+  useEffect(() => document.title = `${completedTodos.length} completed to do's`);
 
   function addTodo(event) {
     event.preventDefault();
@@ -36,6 +27,7 @@ const Todo = () => {
       complete: false
     });
     setTextInput("");
+    sendSuccessMessage(`${textInput} added successfully.`);
   }
 
   function toggleComplete({ id, name, complete }) {
@@ -45,9 +37,11 @@ const Todo = () => {
   }
   function deleteTodo(id) {
     dispatch({ type: "DELETE_TODO", id });
+    sendSuccessMessage(`${props.dataItem.name} was removed successfully.`);
   }
   function clearTodos() {
     dispatch({ type: "CLEAR_TODOS" });
+    sendSuccessMessage(`All todos removed successfully.`);
   }
 
   // function rowRender(trElement, props) {
@@ -69,40 +63,22 @@ const Todo = () => {
     }
   }
 
-  function setScreenReaderMessage(name, action) {
-    return ` ${action} to do item: ${name}`;
+  function setAriaLabel(name, action) {
+    return ` ${action} to do item, ${name}`;
   }
-  //generates the action for the screen reader message based on if the itme is complete or not.
   function generateCompleteAction(dataItem) {
     return dataItem.complete ? "incompletes" : "completes";
   }
-  //sends an alert to the screen reader after an action is complete.
   function sendSuccessMessage(msg) {
     setSuccessMsg(msg);
-    setTimeout(() => {
-      setSuccessMsg("");
-    }, 5000);
+    setTimeout(() => setSuccessMsg(""), 5000);
   }
   return (
     <>
       <div className="todo-form">
         <form onSubmit={addTodo}>
-          <Input
-            onChange={textInputOnChange}
-            value={textInput}
-            type="search"
-            placeholder="Enter task..."
-            autoComplete="off"
-          />
-          <Button
-            onClick={e => {
-              addTodo(e);
-              sendSuccessMessage(`${textInput} added successfully.`);
-            }}
-            look="bare"
-            icon="plus"
-            type="submit"
-          >
+          <Input onChange={textInputOnChange} value={textInput} type="search" placeholder="Enter task..." autoComplete="off" />
+          <Button onClick={e => addTodo(e)} look="bare" icon="plus" type="submit">
             Add To Do
           </Button>
         </form>
@@ -110,62 +86,32 @@ const Todo = () => {
       <div className="todo-container">
         <Grid
           // rowRender={rowRender}
-          data={todos}
-          style={{ width: "100%", height: "100%" }}
+          data={todos} style={{ width: "100%", height: "100%" }}
         >
           <Column field="name" title="Name" />
-          <Column
-            field="complete"
-            title="Completed"
+          <Column field="complete" title="Completed"
             cell={props => (
               <td>
-                <Button
-                  onClick={() => toggleComplete(props.dataItem)}
-                  aria-label={setScreenReaderMessage(
-                    props.dataItem.name,
-                    generateCompleteAction(props.dataItem)
-                  )}
-                  look="bare"
-                  icon={
-                    props.dataItem[props.field]
-                      ? "checkbox-checked"
-                      : "checkbox"
-                  }
+                <Button onClick={() => toggleComplete(props.dataItem)}
+                  aria-label={setAriaLabel(props.dataItem.name, generateCompleteAction(props.dataItem))}
+                  look="bare" icon={props.dataItem[props.field] ? "checkbox-checked" : "checkbox" }
                 />
               </td>
             )}
           />
-          <Column
-            title="Remove"
+          <Column title="Remove"
             cell={props => (
               <td>
-                <Button
-                  onClick={() => {
-                    deleteTodo(props.dataItem.id);
-                    sendSuccessMessage(
-                      `${props.dataItem.name} was removed successfully.`
-                    );
-                  }}
-                  aria-label={setScreenReaderMessage(
-                    props.dataItem.name,
-                    "removes"
-                  )}
-                  look="bare"
-                  icon="trash"
+                <Button onClick={() => deleteTodo(props.dataItem.id)} 
+                  aria-label={setAriaLabel(props.dataItem.name,"removes")} 
+                  look="bare" icon="trash"
                 />
               </td>
             )}
           />
         </Grid>
       </div>
-      <Button
-        look="bare"
-        icon="reset"
-        onClick={() => {
-          clearTodos();
-          sendSuccessMessage(`All todos removed successfully.`);
-        }}
-      >
+      <Button onClick={() => clearTodos()} look="bare" icon="reset">
         Remove All To Do's
       </Button>
       <Alert>{successMsg}</Alert>
