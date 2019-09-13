50dfe29512d1442d7d9bf81735fd340b3716caa5
Eric Bishard <e.bishard@gmail.com>
2019-09-13 20:15:28 +0200

    Commit from React Live coding

--- app/view-components/todos/Todos.js
+++ app/view-components/todos/Todos.js
@@ -11,42 +11,53 @@ import { todoReducer } from "./todoReducer";
 const initialState = [...constants.TODO_SEED];
 
 const Todo = () => {
-  // 01: Add todo and textInput state
-  // 1b: Update Grid data source
-  // 02: Add completedTodos filter
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [textInput, setTextInput] = useState('');
+ 
+  const completedTodos = todos.filter((todo) => todo.complete);
 
-  // 05: Add effect for doc title
+  useEffect(() => {
+    document.title = `${completedTodos.length} completed to do's`;
+  });
 
   function addTodo(event) {
     event.preventDefault();
-    // 06: Add add_todo dispatch
+    dispatch({
+      type: 'ADD_TODO',
+      name: textInput,
+      complete: false
+    });
+    setTextInput('');
   }
   function toggleComplete(id) {
-    // 07: Add toggle_complete dispatch
+    dispatch({ type: 'TOGGLE_COMPLETE', id });
   }
   function deleteTodo(id) {
-    // 08: Add delete_todos dispatch
+    dispatch({ type: 'DELETE_TODO', id });
   }
   function clearTodos() {
-    // 09: Add clear_todos dispatch
+    dispatch({ type: 'CLEAR_TODOS' });
   }
 
-  // 03: Add updateTextInput function
+  function updateTextInput(event) {
+    const value = event.target.value;
+    if (textInput !== value) {
+      setTextInput(value);
+    }
+  }
 
   return (
     <>
       <div className="todo-form">
         <form onSubmit={addTodo}>
-          {/* 04: Add onChange */}
-          <Input type="search" placeholder="Enter task..." autoComplete="off" />
+          <Input onChange={updateTextInput} value={textInput} type="search" placeholder="Enter task..." autoComplete="off" />
           <Button onClick={e => addTodo(e)} look="bare" icon="plus" type="submit">
             Add To Do
           </Button>
         </form>
       </div>
       <div className="todo-container">
-        {/* 1b: Change from initialState to todos */}
-        <Grid data={initialState} style={{ width: "100%", height: "100%" }}>
+        <Grid data={todos} style={{ width: "100%", height: "100%" }}>
           <Column field="name" title="Name" />
           <Column field="complete" title="Completed"
             cell={(props) => (
